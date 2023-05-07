d4872ed96cae1c2ce4480a5396a3058f23149cb4
Hemjaytech <sulaymanmujeeb6@gmail.com>
2023-05-07 09:34:54 -0700

    main

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { heroData } from '../utils/Data';
 import Delivery from '../img/delivery.png';
-import HeroBg from '../img/heroBg.png'
+import cheff from '../img/cheff.png'
 
 
 const HomeContainer = () => {
@@ -30,34 +30,10 @@ const HomeContainer = () => {
                 <button type='button' className='bg-blue-400 w-auto px-4 py-2 rounded-lg mb-12 lg:mb-0 hover:shadow-lg hover:bg-blue-800 transition-all ease-in-out duration-100 text-white'>Order now</button>
             </div>
             <div className='py-2 flex-1 items-center relative'>
-                <img src={HeroBg} alt="Hero-bg" className='ml-auto h-420 w-full lg:w-auto lg:h-650' />
+                <img src={cheff} alt="Hero-bg" className='ml-auto w-full lg:w-auto lg:h-650' />
 
-                <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32 py-4 gap-2 flex-wrap'>
-                    {heroData &&
-                        heroData.map((n) => (
-                            <div
-                                key={n.id}
-                                className="  lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
-                            >
-                                <img
-                                    src={n.imageSrc}
-                                    className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
-                                    alt="I1"
-                                />
-                                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
-                                    {n.name}
-                                </p>
 
-                                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
-                                    {n.decp}
-                                </p>
 
-                                <p className="text-sm font-semibold text-headingColor">
-                                    <span className="text-xs text-red-600">N</span> {n.price}
-                                </p>
-                            </div>
-                        ))}
-                </div>
             </div>
         </div>
     )
