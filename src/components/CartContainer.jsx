2037ee653dd34a6c44492c62931fe8ff47157322
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2024-04-30 15:48:14 +0100

    CartContainer

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,151 +1,152 @@
-import React, { useEffect, useState } from "react";
-import { MdOutlineKeyboardBackspace } from "react-icons/md";
-import { RiRefreshFill } from "react-icons/ri";
+import React, { useEffect, useState } from 'react';
+import { MdOutlineKeyboardBackspace } from 'react-icons/md';
+import { RiRefreshFill } from 'react-icons/ri';
 import { FlutterWaveButton, closePaymentModal } from 'flutterwave-react-v3';
 
-import { motion } from "framer-motion";
-import { useStateValue } from "../context/StateProvider";
-import { actionType } from "../context/reducer";
-import EmptyCart from "../img/emptyCart.svg";
-import CartItem from "./CartItem";
-import { useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion';
+import { useStateValue } from '../context/StateProvider';
+import { actionType } from '../context/reducer';
+import EmptyCart from '../img/emptyCart.svg';
+import CartItem from './CartItem';
+import { useNavigate } from 'react-router-dom';
 
 const CartContainer = () => {
-  const [{ cartShow, cartItems, user }, dispatch] = useStateValue();
-  const [flag, setFlag] = useState(1);
-  const [tot, setTot] = useState(0);
+	const [{ cartShow, cartItems, user }, dispatch] = useStateValue();
+	const [flag, setFlag] = useState(1);
+	const [tot, setTot] = useState(0);
 
-  const showCart = () => {
-    dispatch({
-      type: actionType.SET_CART_SHOW,
-      cartShow: !cartShow,
-    });
-  };
+	const showCart = () => {
+		dispatch({
+			type: actionType.SET_CART_SHOW,
+			cartShow: !cartShow,
+		});
+	};
 
-  useEffect(() => {
-    let totalPrice = cartItems.reduce(function (accumulator, item) {
-      return accumulator + item.qty * item.price;
-    }, 0);
-    setTot(totalPrice);
-  }, [tot, flag]);
+	useEffect(() => {
+		let totalPrice = cartItems.reduce(function (accumulator, item) {
+			return accumulator + item.qty * item.price;
+		}, 0);
+		setTot(totalPrice);
+	}, [tot, flag]);
 
-  const clearCart = () => {
-    dispatch({
-      type: actionType.SET_CARTITEMS,
-      cartItems: [],
-    });
+	const clearCart = () => {
+		dispatch({
+			type: actionType.SET_CARTITEMS,
+			cartItems: [],
+		});
 
-    localStorage.setItem("cartItems", JSON.stringify([]));
-  };
+		localStorage.setItem('cartItems', JSON.stringify([]));
+	};
 
-  const navigate = useNavigate()
+	const navigate = useNavigate();
 
-  const config = {
-    public_key: 'FLWPUBK-de9b0b4d56b3bf16275a5df84076f237-X',
-    tx_ref: Date.now(),
-    amount: tot + 1000,
-    currency: 'NGN',
-    payment_options: 'card,mobilemoney,ussd',
-    customer: {
-      email: 'user@gmail.com',
-      phone_number: '070********',
-      name: 'john doe',
-    },
-    customizations: {
-      title: 'Hem-Destro',
-      description: 'Payment for items in cart',
-      logo: '',
-    },
-  };
+	const config = {
+		public_key: 'FLWPUBK-de9b0b4d56b3bf16275a5df84076f237-X',
+		tx_ref: Date.now(),
+		amount: tot + 1000,
+		currency: 'NGN',
+		payment_options: 'card,mobilemoney,ussd',
+		customer: {
+			email: 'user@gmail.com',
+			phone_number: '070********',
+			name: 'john doe',
+		},
+		customizations: {
+			title: 'Hem-Destro',
+			description: 'Payment for items in cart',
+			logo: '',
+		},
+	};
 
+	const fwConfig = {
+		...config,
+		text: 'Pay with Flutterwave!',
+		callback: (response) => {
+			console.log(response);
+			closePaymentModal(); // this will close the modal programmatically
+		},
+		onClose: () => {},
+	};
 
-  const fwConfig = {
-    ...config,
-    text: 'Pay with Flutterwave!',
-    callback: (response) => {
-      console.log(response);
-      closePaymentModal() // this will close the modal programmatically
-    },
-    onClose: () => { },
-  };
+	return (
+		<motion.div
+			initial={{ opacity: 0, x: 200 }}
+			animate={{ opacity: 1, x: 0 }}
+			exit={{ opacity: 0, x: 200 }}
+			className='fixed top-0 right-0 w-full md:w-375 h-screen bg-white drop-shadow-md flex flex-col z-[101]'>
+			<div className='w-full flex items-center justify-between p-4 cursor-pointer'>
+				<motion.div
+					whileTap={{ scale: 0.75 }}
+					onClick={showCart}>
+					<MdOutlineKeyboardBackspace className='text-textColor text-3xl' />
+				</motion.div>
+				<p className='text-textColor text-lg font-semibold'>Cart</p>
 
+				<motion.p
+					whileTap={{ scale: 0.75 }}
+					className='flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md hover:shadow-md  cursor-pointer text-textColor text-base'
+					onClick={clearCart}>
+					Clear <RiRefreshFill />
+				</motion.p>
+			</div>
 
-  return (
-    <motion.div
-      initial={{ opacity: 0, x: 200 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: 200 }}
-      className="fixed top-0 right-0 w-full md:w-375 h-screen bg-white drop-shadow-md flex flex-col z-[101]"
-    >
-      <div className="w-full flex items-center justify-between p-4 cursor-pointer">
-        <motion.div whileTap={{ scale: 0.75 }} onClick={showCart}>
-          <MdOutlineKeyboardBackspace className="text-textColor text-3xl" />
-        </motion.div>
-        <p className="text-textColor text-lg font-semibold">Cart</p>
+			{/* bottom section */}
+			{cartItems && cartItems.length > 0 ? (
+				<div className='w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col'>
+					{/* cart Items section */}
+					<div className='w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none'>
+						{/* cart Item */}
+						{cartItems &&
+							cartItems.length > 0 &&
+							cartItems.map((item) => (
+								<CartItem
+									key={item.id}
+									item={item}
+									setFlag={setFlag}
+									flag={flag}
+								/>
+							))}
+					</div>
 
-        <motion.p
-          whileTap={{ scale: 0.75 }}
-          className="flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md hover:shadow-md  cursor-pointer text-textColor text-base"
-          onClick={clearCart}
-        >
-          Clear <RiRefreshFill />
-        </motion.p>
-      </div>
+					{/* cart total section */}
+					<div className='w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2'>
+						<div className='w-full flex items-center justify-between'>
+							<p className='text-gray-400 text-lg'>Sub Total</p>
+							<p className='text-gray-400 text-lg'>N {tot}</p>
+						</div>
+						<div className='w-full flex items-center justify-between'>
+							<p className='text-gray-400 text-lg'>Delivery</p>
+							<p className='text-gray-400 text-lg'>N 1000</p>
+						</div>
 
-      {/* bottom section */}
-      {cartItems && cartItems.length > 0 ? (
-        <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
-          {/* cart Items section */}
-          <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
-            {/* cart Item */}
-            {cartItems &&
-              cartItems.length > 0 &&
-              cartItems.map((item) => (
-                <CartItem
-                  key={item.id}
-                  item={item}
-                  setFlag={setFlag}
-                  flag={flag}
-                />
-              ))}
-          </div>
+						<div className='w-full border-b border-gray-600 my-2'></div>
 
-          {/* cart total section */}
-          <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
-            <div className="w-full flex items-center justify-between">
-              <p className="text-gray-400 text-lg">Sub Total</p>
-              <p className="text-gray-400 text-lg">N {tot}</p>
-            </div>
-            <div className="w-full flex items-center justify-between">
-              <p className="text-gray-400 text-lg">Delivery</p>
-              <p className="text-gray-400 text-lg">N 1000</p>
-            </div>
+						<div className='w-full flex items-center justify-between'>
+							<p className='text-gray-200 text-xl font-semibold'>Total</p>
+							<p className='text-gray-200 text-xl font-semibold'>
+								N{tot + 1000}
+							</p>
+						</div>
 
-            <div className="w-full border-b border-gray-600 my-2"></div>
-
-            <div className="w-full flex items-center justify-between">
-              <p className="text-gray-200 text-xl font-semibold">Total</p>
-              <p className="text-gray-200 text-xl font-semibold">
-                N{tot + 1000}
-              </p>
-            </div>
-
-
-            <button className='bg-yellow-500 p-2 rounded-lg text-center my-4 font-bold tracking-wider sansPro'>
-              <FlutterWaveButton {...fwConfig} />
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div className="w-full h-full flex flex-col items-center justify-center gap-6">
-          <img src={EmptyCart} className="w-300" alt="" />
-          <p className="text-xl text-textColor font-semibold">
-            Add some items to your cart
-          </p>
-        </div>
-      )}
-    </motion.div>
-  );
+						<button className='bg-yellow-500 p-2 rounded-lg text-center my-4 font-bold tracking-wider sansPro'>
+							<FlutterWaveButton {...fwConfig} />
+						</button>
+					</div>
+				</div>
+			) : (
+				<div className='w-full h-full flex flex-col items-center justify-center gap-6'>
+					<img
+						src={EmptyCart}
+						className='w-300'
+						alt=''
+					/>
+					<p className='text-xl text-textColor font-semibold'>
+						Add some items to your cart
+					</p>
+				</div>
+			)}
+		</motion.div>
+	);
 };
 
 export default CartContainer;
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,38 +1,49 @@
-import React from 'react'
+import React from 'react';
 import Delivery from '../img/delivery.png';
-import cheff from '../img/cheff.png'
-
+import cheff from '../img/cheff.png';
 
 const HomeContainer = () => {
+	return (
+		<div className='grid grid-cols-1 py-20 md:grid-cols-2 w-full'>
+			<div className='py-2 flex-1 flex flex-col items-start justify-center gap-6'>
+				<div className='flex items-center gap-2 justify-center bg-gray-400 px-2 py-1 rounded-full'>
+					<p className='text-base text-gray-100 font-normal'>Bike Delivery</p>
+					<div className='w-8 h-8 rounded-full overflow-hidden drop-shadow-xl'>
+						<img
+							src={Delivery}
+							alt='Bike Delivery'
+							className='w-full bg-white h-full object-contain w-'
+						/>
+					</div>
+				</div>
+				<p className='text-[2.5rem] lg:text-[4.6rem] font-extrabold tracking-wide text-headingColor'>
+					The fastest Delivery in{' '}
+					<span className='text-blue-400 text-[3rem] lg:text-[4.8rem]'>
+						Ibadan City
+					</span>
+				</p>
+				<p className='text-base text-textColor text-left md:w-[80%]'>
+					Restaurant style. Yogurt Mint Sauce is delicious dip which is quick
+					and easy to .. This is a standard Indian mint chutney served with
+					poppadums along with mint and lemon. We provide pleasure to your
+					tastebuds 😉..
+				</p>
 
+				<button
+					type='button'
+					className='bg-blue-400 w-auto px-4 py-2 rounded-lg mb-12 lg:mb-0 hover:shadow-lg hover:bg-blue-800 transition-all ease-in-out duration-100 text-white'>
+					Order now
+				</button>
+			</div>
+			<div className='flex justify-center items-center relative'>
+				<img
+					src={cheff}
+					alt='Hero-bg'
+					className='flex items-center justify-center lg:h-600 h-300 lg:w-auto'
+				/>
+			</div>
+		</div>
+	);
+};
 
-    return (
-        <div className='grid grid-cols-1 py-20 md:grid-cols-2 w-full'>
-            <div className='py-2 flex-1 flex flex-col items-start justify-center gap-6'>
-                <div className='flex items-center gap-2 justify-center bg-gray-400 px-2 py-1 rounded-full'>
-                    <p className='text-base text-gray-100 font-normal'>
-                        Bike Delivery
-                    </p>
-                    <div className='w-8 h-8 rounded-full overflow-hidden drop-shadow-xl'>
-                        <img
-                            src={Delivery}
-                            alt='Bike Delivery'
-                            className='w-full bg-white h-full object-contain w-'
-                        />
-                    </div>
-                </div>
-                <p className='text-[2.5rem] lg:text-[4.6rem] font-extrabold tracking-wide text-headingColor'>
-                    The fastest Delivery in <span className='text-blue-400 text-[3rem] lg:text-[5rem]'>Ibadan City</span>
-                </p>
-                <p className='text-base text-textColor text-left md:w-[80%]'>Restaurant style. Yogurt Mint Sauce is delicious dip which is quick and easy to .. This is a standard Indian mint chutney served with poppadums along with mint and lemon. We provide pleasure to your tastebuds 😉..</p>
-
-                <button type='button' className='bg-blue-400 w-auto px-4 py-2 rounded-lg mb-12 lg:mb-0 hover:shadow-lg hover:bg-blue-800 transition-all ease-in-out duration-100 text-white'>Order now</button>
-            </div>
-            <div className='flex justify-center items-center relative'>
-                <img src={cheff} alt="Hero-bg" className='flex items-center justify-center lg:h-600 h-300 lg:w-auto' />
-            </div>
-        </div>
-    )
-}
-
-export default HomeContainer
+export default HomeContainer;
